53eab5b3a14875d4cb73164baacad3096319fbd4
Luiz Furtado <luizfpfurtado@hotmail.com>
2024-05-12 17:55:52 -0400

    Challenges implemented

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,23 +2,39 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get => _numero; }
+        public string Modelo { get => _modelo; }
+        public string Imei { get => _imei; }
+        public int Memoria { get => _memoria; }
 
-        public Smartphone(string numero)
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string _modelo;
+        protected string _imei;
+        protected int _memoria;
+        protected string _numero;
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            Numero = numero;
+            _numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
+        }
+
+        public void ModificaNumero(string novoNumero)
+        {
+            _numero = novoNumero;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando do aparelho tipo {GetType().Name}, modelo: {_modelo}...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação no aparelho tipo {GetType().Name}, modelo: {_modelo}...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("11999999999", "Nokia 3210", "123456789", 16);	
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Iphone iphone = new Iphone("11999999999", "Iphone 13", "123456789", 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
